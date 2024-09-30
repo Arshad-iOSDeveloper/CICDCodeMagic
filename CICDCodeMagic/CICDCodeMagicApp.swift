76fe4904af11c2ceecbd15597bbf8b2e4169ad59
Shaik Arshad <2000131696@2000131696-gkq3qtq240.corp.hexaware.com>
2024-09-30 12:50:51 +0530

    First Commit

---
+++ CICDCodeMagic/CICDCodeMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDCodeMagicApp.swift
+//  CICDCodeMagic
+//
+//  Created by Shaik Arshad on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CICDCodeMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDCodeMagic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CICDCodeMagic
+//
+//  Created by Shaik Arshad on 30/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
